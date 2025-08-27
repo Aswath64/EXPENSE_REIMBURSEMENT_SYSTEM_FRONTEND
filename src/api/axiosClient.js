0d63a89f255cd1217aa7aba2da25c55ae76a3cdb
Aswath64 <144975456+Aswath64@users.noreply.github.com>
2025-08-27 16:20:34 +0530

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://expensereimbursementsystembackend-production.up.railway.app/api',
 });
 
 axiosClient.interceptors.request.use(config => {
